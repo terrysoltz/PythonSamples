a82f29dd5e19eea3c110591e431cb079acb77e81
terrysoltz <terrence.soltz@gmail.com>
2017-02-22 18:31:42 -0800

    drill for step 46
    explores use of range() function

---
+++ PythonDrill46.py
@@ -0,0 +1,17 @@
+# Python drill for step 46
+#
+# Python Version: 3.6.0
+#
+# Author: Terry Soltz
+#
+# Purpose: To explore use of the range() function
+
+
+for i in range(4):
+    print(i)
+
+for i in range(3, -1, -1):
+    print(i)
+
+for i in range(8, 0, -2):
+    print(i)
